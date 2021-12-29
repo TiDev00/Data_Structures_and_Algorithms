20923bd70c22ed712a40ba86a329cc9dc0413d9c
TiDev00 <cisse.thierno@outlook.com>
2021-12-29 05:04:33 -0500

    Sum of 2 digits

---
+++ Programming challenges/Sum_of_2_digits.py
@@ -0,0 +1,11 @@
+print("\t**********************************************")
+print("\t\t\t\t***  SUM OF 2 DIGITS  ***")
+print("\t**********************************************")
+
+answer = (input("What are the 2 numbers: ").split(" "))
+num_list = ([int(number) for number in answer])
+if len(num_list) < 2:
+    print("You gave one number")
+else:
+    print("The sum of " + str(num_list[0]) + " and " + str(num_list[1]) + " is", num_list[0] + num_list[1])
+

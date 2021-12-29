756d12667ed38872dd07cd7d454aabc6a0f1d273
TiDev00 <cisse.thierno@outlook.com>
2021-12-29 06:03:13 -0500

    Sum of 2 digits by function

--- Programming challenges/Sum_of_2_digits.py
+++ Programming challenges/Sum_of_2_digits.py
@@ -1,11 +1,9 @@
-print("\t**********************************************")
-print("\t\t\t\t***  SUM OF 2 DIGITS  ***")
-print("\t**********************************************")
 
-answer = (input("What are the 2 numbers: ").split(" "))
-num_list = [int(number) for number in answer]
-if len(num_list) < 2:
-    print("You gave one number")
-else:
-    print("The sum of " + str(num_list[0]) + " and " + str(num_list[1]) + " is", num_list[0] + num_list[1])
+def sum(a, b):
+    return a+b
+
+if __name__ == '__main__':
+    answer = (input().split(" "))
+    num_list = [int(number) for number in answer]
+    print(sum(num_list[0], num_list[1]))
 

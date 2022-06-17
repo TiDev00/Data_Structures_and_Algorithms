f8a69ead7a775210d2edb7f79cd5df2ac0173032
TiDev00 <cisse.thierno@outlook.com>
2022-06-17 15:55:15 -0400

    directory update

--- Programming_Challenges/Sum_of_2_digits.py
+++ Programming_Challenges/Sum_of_2_digits.py
@@ -1,8 +1,8 @@
+def digits_sum(a, b):
+    return a + b
 
-def sum(a, b):
-    return a+b
 
 if __name__ == '__main__':
     answer = (input().split(" "))
     num_list = [int(number) for number in answer]
-    print(sum(num_list[0], num_list[1]))
+    print(digits_sum(num_list[0], num_list[1]))

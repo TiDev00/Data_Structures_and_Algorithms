7c78d9446656f735de7ca409923ea1549acc894c
TiDev00 <cisse.thierno@outlook.com>
2022-06-23 03:12:09 -0400

    5th_impl

--- Programming_Challenges/Max_pairwise_product.py
+++ Programming_Challenges/Max_pairwise_product.py
@@ -1,14 +1,13 @@
-def max_product(nb_element, elements):
-    product = 0
-    for first_index in range(nb_element):
-        for second_index in range(first_index + 1, nb_element):
-            product = max(product, elements[first_index] * elements[second_index])
-    return product
+def max_product(elements):
+    first_max = max(elements)
+    elements.remove(first_max)
+    second_max = max(elements)
+    return first_max * second_max
 
 
 if __name__ == '__main__':
     input_n = int(input())
     enter = (input().split(" "))
     num_list = [int(number) for number in enter]
-    print(max_product(input_n, num_list))
+    print(max_product(num_list))
 

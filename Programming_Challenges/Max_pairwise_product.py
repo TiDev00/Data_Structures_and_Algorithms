d138b108d28c41d8681f5d313e3e285954133e09
TiDev00 <cisse.thierno@outlook.com>
2022-06-23 01:40:19 -0400

    first_impl

---
+++ Programming_Challenges/Max_pairwise_product.py
@@ -0,0 +1,15 @@
+import time
+
+
+def max_product(elements):
+    max_nb1 = max(elements)
+    max_nb2 = 0
+    for element in elements:
+        if max_nb1 > element > max_nb2:
+            max_nb2 = element
+    return max_nb1 * max_nb2
+
+if __name__ == '__main__':
+    enter = (input().split(" "))
+    num_list = [int(number) for number in enter]
+    print(max_product(num_list))
